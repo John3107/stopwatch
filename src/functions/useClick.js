5468bd8adf86202313b26d5ae31006cccc675167
John3107 <maynsayt@gmail.com>
2021-12-31 01:31:46 +0200

    300ms

--- src/functions/useClick.js
+++ src/functions/useClick.js
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
-import { buffer, filter, tap, map, debounceTime } from 'rxjs';
+import { buffer, filter, map, asyncScheduler } from 'rxjs';
+import { throttleTime } from "rxjs/operators";
 
 export function useClick(mouseClicks$, setState) {
     useEffect(() => {
       if (!mouseClicks$) return;
       const subject$ = mouseClicks$
         .pipe(
-          buffer(mouseClicks$.pipe(debounceTime(300))),
-          tap((e) => console.log(e)),
+          buffer(mouseClicks$.pipe(throttleTime(300, asyncScheduler, {
+            leading: false,
+            trailing: true,
+          }))),
           map((e) => e.length),
           filter((e) => e === 2),
         )
--- src/functions/useObservable.js
+++ src/functions/useObservable.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import {fromEvent} from 'rxjs';
+import { fromEvent } from 'rxjs';
 
 export function useObservable(ref, event) {
-    const [subject$, setSubject$] = useState();
-    useEffect(() => {
-      if (!ref.current) return;
-      setSubject$(fromEvent(ref.current, event));
-    }, [ref, event]);
-    return subject$;
-  }
+  const [subject$, setSubject$] = useState();
+  useEffect(() => {
+    if (!ref.current) return;
+    setSubject$(fromEvent(ref.current, event));
+  }, [ref, event]);
+  return subject$;
+}
